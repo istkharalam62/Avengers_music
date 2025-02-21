4068df21240741a0140802981c3bb66a81b3abcc
rambahi <pk0031641@gmail.com>
2025-02-21 21:23:32 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -40,21 +40,21 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "—————————"
+        bar = "❤️‍🔥—————————"
     elif 10 < umm < 20:
-        bar = "—————————"
+        bar = "——❤️‍🔥———————"
     elif 20 <= umm < 30:
-        bar = "—————————"
+        bar = "———❤️‍🔥——————"
     elif 30 <= umm < 40:
-        bar = "—————————"
+        bar = "—————❤️‍🔥————"
     elif 40 <= umm < 50:
-        bar = "—————————"
+        bar = "——————❤️‍🔥———"
     elif 50 <= umm < 60:
-        bar = "—————————"
+        bar = "———————❤️‍🔥——"
     elif 60 <= umm < 70:
-        bar = "—————————"
+        bar = "—————————❤️‍🔥"
     elif 70 <= umm < 80:
-        bar = "——Jani—Music——"
+        bar = "——————————"
     elif 80 <= umm < 95:
         bar = "——𝐕𝐢𝐏—𝐉𝐚𝐧𝐢——"
     else:
@@ -76,10 +76,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="✰ 𝖡ᴧ፝֠֩ʙꭎ ✰", url="http://t.me/RAM_HACKER_RP",
+                text="✰ 𝖡ᴧ፝֠֩ʙꭎ ✰", url="http://t.me/AVENGERS_OWNER",
             ),
             InlineKeyboardButton(
-                text="✰ 𝛅ᴏ፝֠֩𝛈ᴧ ✰", url="https://t.me/VIP_RAM_HACKER_PR",
+                text="✰ 𝛅ᴏ፝֠֩𝛈ᴧ ✰", url="https://t.me/II_WORLD_CHAT_II",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -99,10 +99,10 @@ def stream_markup(_, videoid, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="✰ 𝖡ᴧ፝֠֩ʙꭎ ✰", url="http://t.me/RAM_HACKER_RP",
+                text="✰ 𝖡ᴧ፝֠֩ʙꭎ ✰", url="http://t.me/AVENGERS_OWNER",
             ),
             InlineKeyboardButton(
-                text="✰ 𝛅ᴏ፝֠֩𝛈ᴧ ✰", url="https://t.me/VIP_RAM_HACKER_PR",
+                text="✰ 𝛅ᴏ፝֠֩𝛈ᴧ ✰", url="https://t.me/II_WORLD_CHAT_II",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
