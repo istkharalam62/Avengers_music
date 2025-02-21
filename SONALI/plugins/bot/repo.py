ec4eab1dcceac9e630efe6cf714f9a1b18826db3
rambahi <pk0031641@gmail.com>
2025-02-21 21:29:05 +0530

    Update repo.py

--- SONALI/plugins/bot/repo.py
+++ SONALI/plugins/bot/repo.py
@@ -13,7 +13,7 @@ start_txt = """
  
 ✰ 𝗣ᴀʜʟᴇ 𝗣ᴀᴘᴀ 𝗕ᴏʟ 𝗥ᴇᴘᴏ 𝗢ᴡɴᴇʀ ᴋᴏ 
 
-✰  @VIP_RAM_HACKER_PR  
+✰  @II_WORLD_CHAT_II  
  
 ✰ 𝗥ᴜɴ 24x7 𝗟ᴀɢ 𝗙ʀᴇᴇ 𝗪ɪᴛʜᴏᴜᴛ 𝗦ᴛᴏᴘ
  
@@ -29,14 +29,14 @@ async def start(_, msg):
           InlineKeyboardButton("𝗔ᴅᴅ ᴍᴇ 𝗠ᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗛ᴇʟᴘ", url="http://t.me/RAM_HACKER_RP"),
-          InlineKeyboardButton("𝐕𝐢𝐏 𝗝𝗮𝗻𝗶", url="https://t.me/VIP_RAM_HACKER_PR"),
+          InlineKeyboardButton("𝗛ᴇʟᴘ", url="http://t.me/AVENGERS_OWNER"),
+          InlineKeyboardButton("𝐕𝐢𝐏", url="https://t.me/II_WORLD_CHAT_II"),
           ],
                [
-                InlineKeyboardButton("𝗝𝗮𝗻𝗶 𝗕ᴏᴛs", url=f"https://t.me/VIP_RAM_HACKER_PR"),
+                InlineKeyboardButton("𝗝𝗮𝗻𝗶 𝗕ᴏᴛs", url=f"https://t.me/II_WORLD_CHAT_II"),
 ],
 [
-InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/VIP_RAM_HACKER_PR"),
+InlineKeyboardButton("𝗠ᴀɪɴ 𝗕ᴏᴛ", url=f"https://t.me/II_WORLD_CHAT_II"),
 
         ]]
 
